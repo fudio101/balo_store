bc3ec79b876467d179d54c20dacfdf88c97d79c0
fudio101 <thenguyen1024@gmail.com>
2021-11-17 16:16:32 +0700

    fix config.php

---
+++ database/config.php
@@ -0,0 +1,6 @@
+<?php
+define('HOST', 'localhost');
+define('DATABASE', 'balostore19');
+define('USERNAME', 'root');
+define('PASSWORD', 'Ng01637202484');   // set password
+define('PRIVATE_KEY', 'sjdgfsdj(*&*&6234jhsdgfjhsdsdfk&*^UUUdd');
