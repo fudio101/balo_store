25b633c93861c70bc3c8a52b4acd48eb49bb5bc0
fudio101 <thenguyen1024@gmail.com>
2021-11-18 23:34:14 +0700

    final fixx

--- layouts/head.php
+++ layouts/head.php
@@ -6,7 +6,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="./assets/images/store-solid.svg" />
     <!--[if lt IE 9]>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.1/respond.js"></script>
         <![endif]-->
